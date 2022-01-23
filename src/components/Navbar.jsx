5c8e9b91a3057cb172bd525e37a962e0103dda15
unknown <Chamon562@gmail.com>
2022-01-23 12:28:47 -0500

    [Navbar.jsx] Add Menu.Item with icon FundOutlined from ant design and link inside to cryptocurencies.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,6 +28,9 @@ const Navbar = () => {
         <Menu.Item icon={<HomeOutlined />}>
           <Link to="/">Home</Link>
         </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </Menu.Item>
         
       </Menu>
     </div>
