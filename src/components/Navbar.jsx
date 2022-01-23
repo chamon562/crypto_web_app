a397ecbca955a0cbbdaadb578df86d1b1a9ed729
unknown <Chamon562@gmail.com>
2022-01-22 22:23:25 -0500

    [Navbar.jsx] Add src and size in Avatar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,13 +13,11 @@ const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar />
+        <Avatar src={icon} size="large"/>
         <Typography.Title level={2} className="logo">
-            <Link to="/">
-                Crypotomatic
-            </Link>
-            {/* button will be used to switch between the menus on mobile devices */}
-            {/* <Button className="menu-control-container">
+          <Link to="/">Crypotomatic</Link>
+          {/* button will be used to switch between the menus on mobile devices */}
+          {/* <Button className="menu-control-container">
 
             </Button> */}
         </Typography.Title>
