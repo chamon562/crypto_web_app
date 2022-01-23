8f4513d19a28d104be974b3047542012adc89054
unknown <Chamon562@gmail.com>
2022-01-23 12:47:13 -0500

    [src/index.js] Add import 'antd/dist/antd.css' to get styleing working from ant design

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom/cjs/react-router-dom.min";
-import App from "./App";
 
+import App from "./App";
+import 'antd/dist/antd.css'
 ReactDOM.render(
   <Router>
     <App />
