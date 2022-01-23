1d6e07e618e4f136da1338820f8b96ae761ee998
unknown <Chamon562@gmail.com>
2022-01-23 12:27:32 -0500

    [Navbar.jsx] Add Menu.Item tag to have the HomeOutlined icon from ant design, and has Link tag going to home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd'
 import {Navbar} from './components';
 import './App.css'
+
 const App = () => {
   // className will be for layout and minor styling changes
   // all main styling will be done specifically through ant design
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,13 @@ import {
   FundOutlined,
   MenuOutlined,
 } from "@ant-design/icons/lib/icons";
-import icon from '../images/cryptomaticLogo.png'
+import icon from "../images/cryptomaticLogo.png";
 
 const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar src={icon} size="large"/>
+        <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
           <Link to="/">Crypotomatic</Link>
           {/* button will be used to switch between the menus on mobile devices */}
@@ -23,6 +23,13 @@ const Navbar = () => {
             </Button> */}
         </Typography.Title>
       </div>
+      {/* Menu tag comes from ant design, and inside can have different Menu items */}
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        
+      </Menu>
     </div>
   );
 };
