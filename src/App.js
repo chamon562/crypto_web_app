f4bd6da24c983503e6bb95f548d4a3763e8e27f2
unknown <Chamon562@gmail.com>
2022-01-23 17:13:50 -0500

    [App.js] Add footer styling in App.css, Space tag along with Link for home in footer

--- src/App.js
+++ src/App.js
@@ -1,55 +1,69 @@
-import React from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
-import {Layout, Typography, Space} from 'antd'
-import {Navbar, Homepage, Cryptocurrencies, CryptoDetails, Exchanges, News} from './components';
-import './App.css'
-import { HomeFilled } from '@ant-design/icons/lib/icons';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  Exchanges,
+  News,
+} from "./components";
+import "./App.css";
+import { HomeFilled } from "@ant-design/icons/lib/icons";
 
 const App = () => {
   // className will be for layout and minor styling changes
   // all main styling will be done specifically through ant design
-  return <div className='app'>
-      <div className='navbar'>
+  return (
+    <div className="app">
+      <div className="navbar">
         <Navbar />
       </div>
-      <div className='main'>
-      {/* Layout component from ant design  */}
-      <Layout>
-        <div className='routes'>
-          {/* Switch allows you to have multiple routes */}
-          <Switch>
+      <div className="main">
+        {/* Layout component from ant design  */}
+        <Layout>
+          <div className="routes">
+            {/* Switch allows you to have multiple routes */}
+            <Switch>
               <Route exact path="/">
-                  {/* provide component want to render under the route */}
+                {/* provide component want to render under the route */}
                 <Homepage />
-              </Route>  
+              </Route>
               <Route exact path="/cryptocurrencies">
-                  {/* provide component want to render under the route */}
+                {/* provide component want to render under the route */}
                 <Cryptocurrencies />
-              </Route>  
+              </Route>
               <Route exact path="/exchanges">
-                  {/* provide component want to render under the route */}
+                {/* provide component want to render under the route */}
                 <Exchanges />
-              </Route>  
+              </Route>
               <Route exact path="/crypto/:coinId">
-                  {/* provide component want to render under the route */}
+                {/* provide component want to render under the route */}
                 <CryptoDetails />
-              </Route>  
+              </Route>
               <Route exact path="/news">
-                  {/* provide component want to render under the route */}
+                {/* provide component want to render under the route */}
                 <News />
-              </Route>  
-          </Switch>
-        </div>
-      </Layout>
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
       </div>
-      <div className='footer'>
-        <Typography.Title>
-          Cryptomatic <br/>
+      <div className="footer">
+        <Typography.Title
+          level={5}
+          style={{ color: "white", textAlign: "center" }}
+        >
+          Cryptomatic <br />
           All rights reserved {new Date().getFullYear()}
         </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+        </Space>
       </div>
-      
-  </div>;
+    </div>
+  );
 };
 
 export default App;
