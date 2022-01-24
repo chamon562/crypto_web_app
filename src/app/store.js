77fc8c8dd0bb5bfbc56f899eadcbefe5cbcae5d3
unknown <Chamon562@gmail.com>
2022-01-24 10:53:50 -0500

    [store.js] Add import {cryptoApi} and add reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit';
 // use in src/index.js
 
+// connecting api to store
+// inside reducer add [cryptoApi.reduerPath]: cryptoApi.reducer,
+// redux will handle everything else, just have to specify that for
+// every single reducer thats created
+import { cryptoApi } from '../services/cryptoApi';
+
 // first paramter is reducer equal to an empty object
 export default configureStore({
-    reducer: {},
+    reducer: {
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+    },
 })
