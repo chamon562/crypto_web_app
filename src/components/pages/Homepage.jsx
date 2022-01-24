94280a0ac169e056f7b0e70e1892e1c900e632cd
unknown <Chamon562@gmail.com>
2022-01-24 11:59:33 -0500

    [Homepage.jsx] Add Cryptocurrencies component under Top 10 div. Copy and paste the same div above with the Component and replaced with News component to show users the latest Crypto news.

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -5,6 +5,7 @@ import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 // import useGetCryptosQuery
 import { useGetCryptosQuery } from "../../services/cryptoApi";
+import { Cryptocurrencies, News } from "..";
 
 const { Title } = Typography;
 // need data for Statistics
@@ -53,6 +54,12 @@ const Homepage = () => {
         <Title level={2} className="home-title" >Top 10 Cryptocurrencies in the world</Title>
         <Title level={3} className="show-more" ><Link to="/cryptocurrencies">Show More</Link></Title>
       </div>
+      <Cryptocurrencies />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title" >Latest Crypto News</Title>
+        <Title level={3} className="show-more" ><Link to="/news">Show More</Link></Title>
+      </div>
+      <News />
     </>
   );
 };
